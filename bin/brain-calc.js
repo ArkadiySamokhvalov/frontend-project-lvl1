241c58d23c86b2c3d5fa62ee4b27060f7ad770bd
ArkadiySamokhvalov <arkadii.samohvalov@bk.ru>
2022-05-25 19:22:51 +0300

    Feat: add new game

---
+++ bin/brain-calc.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import { init } from '../src/index.js';
+import startGame from '../src/games/calc.js';
+
+init();
+startGame();
---
+++ src/games/calc.js
@@ -0,0 +1,51 @@
+import * as game from '../index.js';
+
+const gameSettings = {
+  rules: 'What is the result of the expression?',
+  int_min: 1,
+  int_max: 10,
+  operators: ['-', '+', '*'],
+  operations: {
+    '-': (a, b) => a - b,
+    '+': (a, b) => a + b,
+    '*': (a, b) => a * b,
+  },
+};
+
+const getRandomOperation = () => {
+  const lastElemIndex = gameSettings.operators.length - 1;
+  return gameSettings.operators[game.getRandomInt(0, lastElemIndex)];
+};
+
+const getCorrectAnswer = (operator, number1, number2) => gameSettings.operations[operator](number1, number2);
+
+const startGame = (rounds = 0) => {
+  if (rounds === 0) game.printText(gameSettings.rules);
+
+  const number1 = game.getRandomInt(gameSettings.int_min, gameSettings.int_max);
+  const number2 = game.getRandomInt(gameSettings.int_min, gameSettings.int_max);
+  const operation = getRandomOperation();
+  const formula = `${number1} ${operation} ${number2}`;
+  game.printQuestion(formula);
+
+  const userAnswer = Number(game.askAnswer());
+  const correctAnswer = Number(getCorrectAnswer(operation, number1, number2));
+
+  if (userAnswer === correctAnswer) {
+    game.printText(game.globalSettings.success);
+    const newRounds = rounds + 1;
+
+    if (game.checkRoundCount(newRounds)) {
+      return game.printText(game.globalSettings.win);
+    }
+
+    startGame(rounds);
+  } else {
+    game.printText(game.globalSettings.fail(userAnswer, correctAnswer));
+    game.printText(game.globalSettings.loss);
+  }
+
+  return null;
+};
+
+export default startGame;
